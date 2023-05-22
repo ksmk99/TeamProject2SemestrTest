5f3c8371106d7b29b28326a8919cb248d326f04a
Nagibator1488-SS <72550914+Nagibator1488-SS@users.noreply.github.com>
2023-05-22 21:28:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from transformers import pipeline, set_seed
+"""Module for create sentences"""
 from deep_translator import GoogleTranslator
+"""Module for translate text"""
 import streamlit as st
+"""Module for create interface"""
 
 
 def translate_text_to_en(text):
+    """Function translate text to English"""
     return GoogleTranslator(source='auto', target='en').translate(text)
 
 
 def translate_text_to_ru(text):
+    """Function translate text to Russian"""
     return GoogleTranslator(source='en', target='ru').translate(text)
 
 
 def generate_en_text(text, length):
+    """Function generate English text"""
     generator = pipeline('text-generation', model='gpt2')
     set_seed(42)
     txt = generator(text, max_length=length,
@@ -21,6 +27,7 @@ def generate_en_text(text, length):
 
 
 def generate_ru_text(text, length):
+    """Function generate Russian text"""
     en_text = translate_text_to_en(text)
     generated_en_text = generate_en_text(en_text, length)
     generated_ru_text = translate_text_to_ru(generated_en_text)
